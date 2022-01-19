715621a4c161a0f8f39c292961a9c689c353c52b
chiquetaMariano <mariano_cr90@hotmail.com>
2022-01-18 23:40:54 -0300

    Complete to-dos state setting feature
    Mark/unmark to-dos as completed.

--- backend/src/services/TodoService.ts
+++ backend/src/services/TodoService.ts
@@ -1,14 +1,21 @@
 export interface Todo {
+    id?: number;
     description: string;
     isCompleted: boolean;
 }
 
 export default class TodoService implements Todo {
+    id?: number;
     description: string;
     isCompleted: boolean;
 
-    constructor(description: string, isCompleted: boolean = false) {
+    constructor(description: string, isCompleted: boolean = false, id?: number) {
         this.description = description;
         this.isCompleted = isCompleted;
+        this.id = id;
+    }
+
+    toggleState(): void {
+        this.isCompleted = !this.isCompleted;
     }
 }
