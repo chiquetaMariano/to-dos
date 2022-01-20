900e35a4c2237c060a542cce6a529b35d834ada6
chiquetaMariano <mariano_cr90@hotmail.com>
2022-01-20 15:33:19 -0300

    Add Login method to AuthService

--- backend/src/services/AuthService.ts
+++ backend/src/services/AuthService.ts
@@ -1,23 +1,63 @@
 import * as argon2 from "argon2";
+import * as jwt from "jsonwebtoken";
 import { randomBytes } from "crypto";
 import db from "../../models";
+import { UserAttributes } from "../../models/user";
 
 export default class AuthService {
-    public async Signup(name: string, email: string, password: string): Promise<any> {
+    public async Signup(fullname: string, email: string, password: string): Promise<any> {
         const salt = randomBytes(32);
         const passwordHashed = await argon2.hash(password, {salt});
 
-        const userRecord = await db.User.create({
-            name,
+        // TODO: check email availability
+
+        const user = await db.User.create({
+            fullname,
             email,
             password: passwordHashed
         });
 
         return {
             user: {
-                name: userRecord.name,
-                email: userRecord.email
+                fullname: user.fullname,
+                email: user.email
             }
         }
+    }
+
+    public async Login(email: string, password: string): Promise<any> {
+        const user = await db.User.findOne({ email });
+        if(!user) {
+            throw new Error("User not found");
+        }
+        else  {
+            const correctPassword = await argon2.verify(user.password, password);
+            if(!correctPassword) {
+                throw new Error("Incorrect password");
+            }
+        }
+
+        return {
+            user: {
+                fullname: user.fullname,
+                email: user.email
+            },
+            token: this.generateJWT(user)
+        }
+    }
+
+    private generateJWT(user: UserAttributes) {
+
+        const data = {
+            id: user.id,
+            fullname: user.fullname,
+            email: user.email
+        };
+
+        // TODO: move secret to .env file
+        const signature = "my super secret";
+        const expiration = "6h";
+
+        return jwt.sign({data}, signature, { expiresIn: expiration });
     }
 }
